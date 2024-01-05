f8e8b2f23d4d7f2a560e4d1539fcb13e1aff289b
dlions9 <dineshgaddipati@gmail.com>
2024-01-05 06:36:31 -0500

    --Added Villa API Controller 1) GetVillas 2)PostVilla 3)GetSingle VIlla

---
+++ VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using VillaAPI.Data;
+using VillaAPI.Models;
+using VillaAPI.Models.DTO;
+
+namespace VillaAPI.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        public  IActionResult GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult GetVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            VillaDTO villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villa)
+        {
+            if (villa == null)
+            {
+                return BadRequest(villa);
+            }
+            if(villa.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villa.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villa);
+            return Ok(villa);
+        }
+
+    }
+}
---
+++ VillaAPI/Models/DTO/VillaDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace VillaAPI.Models.DTO
+{
+    public class VillaDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+
+    }
+}
